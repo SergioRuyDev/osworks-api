f5facabc96ac53dbbc96f44b9c14354228a8fe12
Sergio Ruy <sergioruyenator@gmail.com>
2020-06-25 11:03:25 -0300

    Persistência de dados adicionada, primeira migração de dados com flyway.

--- src/main/java/com/sergioruy/osworksapi/controller/ClienteController.java
+++ src/main/java/com/sergioruy/osworksapi/controller/ClienteController.java
@@ -14,13 +14,13 @@ public class ClienteController {
     public List<Cliente> listar() {
         var cliente1 = new Cliente();
         cliente1.setId(1L);
-        cliente1.setNome("João");
+        cliente1.setNome("João das couves");
         cliente1.setTelefone("34 99999-1111");
         cliente1.setEmail("joaodascoves@gmail.com");
 
         var cliente2 = new Cliente();
-        cliente2.setId(1L);
-        cliente2.setNome("Maria");
+        cliente2.setId(2L);
+        cliente2.setNome("Maria Clara");
         cliente2.setTelefone("34 88888-2222");
         cliente2.setEmail("mariaclara@gmail.com");
 
